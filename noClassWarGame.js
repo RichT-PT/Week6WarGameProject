87341fa87e298d6173881f5250fe15e4e938d858
RichT-PT <diverdwn06@outlook.com>
2023-02-01 10:25:35 -0600

    added copied code and question anchor

--- noClassWarGame.js
+++ noClassWarGame.js
@@ -40,3 +40,35 @@ var p2Score = 0;
     }
     //ANCHOR - the code below was copied from a friend and I would like to understand its' function better
     // also, will the value stay with the cards if you attach the card to a suit in a nested for loop?
+
+    const SUITS = ["♥", "♠", "♦", "♣"]
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+]
+const CARD_VALUE_MAP = {
+  "2":2,
+  "3":3,
+  "4":4,
+  "5":5,
+  "6":6,
+  "7":7,
+  "8":8,
+  "9":9,
+  "10":10,
+  "J":11,
+  "Q":12,
+  "K":13,
+  "A":14
+}
